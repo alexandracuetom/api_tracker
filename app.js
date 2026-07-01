97d2d84d23b6e28b028ec8680a99154366d4b1b3
Alexandra Cueto Merayo <alexandrabcm5@gmail.com>
2026-07-01 12:36:08 +0200

    Error handling middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscriptions.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -13,6 +15,11 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+app.use(errorMiddleware);
+//Built-in middlewares from express
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription tracker API');
