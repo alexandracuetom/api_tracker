6724746e35b0a77e9880c1351ac70c6a8823f4e0
Alexandra Cueto Merayo <alexandrabcm5@gmail.com>
2026-07-04 12:56:53 +0200

    Arcjet security against bots and spamming added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from './routes/subscriptions.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middleware/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middleware/arcjet.middleware.js'
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(errorMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
